9c5acb9c22ccc4cdc022176888a62fd41d51ee22
Anup S Nair <learnanup@gmail.com>
2022-06-09 16:28:45 +0530

    Update streamlit_app.py
    updated streamline to streamlit - syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let’s put a pick list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-
-# Let’s put a pick list here so they can pick the fruit they want to include
-streamline.multiselect( "Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+#Let’s put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect( "Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
