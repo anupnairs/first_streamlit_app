5cc577cdf8ffc33f27d9006ce4396075e2d5c089
Anup S Nair <learnanup@gmail.com>
2022-06-09 16:07:23 +0530

    Update streamlit_app.py
    Update the text list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast menu')
 streamlit.text ('Omega 3')
+streamlit.text ('Egg and Milk')
+streamlit.text ('Idli')
+streamlit.text ('Dosa')
